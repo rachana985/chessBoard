b3f48f89c2d9615523a53f4e8418b783484b2dc9
rachanabheemreddy <rachanareddy.work@gmail.com>
2021-08-02 13:05:23 -0400

    Chess Board

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Table from './components/table';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center mt-2">Chess Layout Table</h1>
+      <Table />
+    </>
+  );
+}
+
+export default App;
